46c3604aace5cbfd461bd96bb20031d0f90d7f7c
Bala K <balak1696@gmail.com>
2026-07-21 14:46:48 +0530

    API ADD code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjust if backend port differs
+    baseURL: 'https://college-timetable-management-system.onrender.com/api', // Adjust if backend port differs
     withCredentials: true,
 });
 
